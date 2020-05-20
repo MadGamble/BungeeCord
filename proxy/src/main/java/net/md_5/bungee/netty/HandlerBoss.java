6273d915a420e95b845e7a487c95a53f1266f121
Heddo <admin@heddo.eu>
2020-05-20 17:53:27 +0200

    fix else -> HandlerBoss

--- proxy/src/main/java/net/md_5/bungee/netty/HandlerBoss.java
+++ proxy/src/main/java/net/md_5/bungee/netty/HandlerBoss.java
@@ -111,7 +111,8 @@ public class HandlerBoss extends ChannelInboundHandlerAdapter
                 if ( sendPacket )
                 {
                     handler.handle( packet );
-                } else {
+                } else 
+                {
                 	if ( packet == null || packet.packet == null )
                 	{
                 		ProxyServer.getInstance().getLogger().log( Level.FINE, "Packet null -> Close channel" );
